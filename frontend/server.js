3fb47b2bbb57fb19967341fe25252b4e3281f914
a22joslarfer <a22joslarfer@inspedralbes.cat>
2024-04-17 10:52:09 +0200

    llistat de sockets connectats, no mostrar processos de nuxt o reconneccions ssr, només usuaris.

--- frontend/server.js
+++ frontend/server.js
@@ -18,61 +18,35 @@ const io = new Server(server, {
   cors: corsOptions,
 });
 
-let seats = [];
-let connected = 0;
-
-// Handle new connections
+let users = [];
 io.on('connection', (socket) => {
+  // Ignore connections from Nuxt HMR
+  if (socket.request.headers.referer && socket.request.headers.referer.includes('hot-update.json')) {
+    return;
+  }
+
   console.log('CONNECTION:', {
     id: socket.id,
     connected: socket.connected,
-
-  });
-  connected++;
-  io.emit('updateConnected', connected);
-  console.log('USERS CONNECTED N:' + connected);
-  socket.on('msg', (data) => {
-    console.log('MSG:', data);
-    io.emit('msg', data);
   });
 
-
-
-  socket.on('addSeatToSeverSeatsArray', (data) => {
-    // afegeix un objecte amb socket.id i seat.id a l'array
-    seats.push({ socketId: socket.id, seatId: data });
-    // envia al client un array amb els seat.id dels seients reservats
-    console.log('Selected seats:', JSON.stringify(seats, null, 2));
-    io.emit('updateSeats', seats);
+  socket.on('userJoined', (userId) => {
+    users[userId] = socket.id;
+    io.emit('updateConnectedUsers', Object.keys(users));
   });
 
-  socket.on('removeSeatFromServerSeatsArray', (data) => {
-    // filtra l'array per eliminar l'objecte amb socket.id i seat.id iguals a data
-    seats = seats.filter(seat => seat.socketId !== socket.id || seat.seatId !== data);
-    // envia al client un array amb els seat.id dels seients reservats
-    console.log('Selected seats:', JSON.stringify(seats, null, 2));
-    io.emit('updateSeats', seats);
-  });
-
-
-  
-
-
-  // Handle disconnections
   socket.on('disconnect', () => {
     console.log('DISCONNECTION:', {
       id: socket.id,
       disconnected: socket.disconnected,
     });
-    connected--;
-    // Elimina el contigut de l'array seats del socket que s'ha desconnectat
-    seats = seats.filter(seat => seat.socketId !== socket.id);
-    console.log('Selected seats:', JSON.stringify(seats, null, 2));
-    io.emit('updateSeats', seats);
-    io.emit('updateConnected', connected);
+
+    // Remove the disconnected user
+    users = Object.fromEntries(Object.entries(users).filter(([userId, socketId]) => socketId !== socket.id));
+    io.emit('updateConnectedUsers', Object.keys(users));
   });
 });
 
-server.listen(3113, () => {
-  console.log('server running at http://localhost:3113');
+server.listen(5000, () => {
+  console.log('server running at http://localhost:5000');
 });
