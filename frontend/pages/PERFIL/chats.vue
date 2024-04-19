dd627e833fb41b4e45613760e2a80fae4c428bbe
a22joslarfer <a22joslarfer@inspedralbes.cat>
2024-04-19 09:35:09 +0200

    chat privat prototip acabat

--- frontend/pages/PERFIL/chats.vue
+++ frontend/pages/PERFIL/chats.vue
@@ -1,8 +1,31 @@
 <template>
-    <div>
+    <div id="container">
         <ul>
-            <li v-for="userId in connectedUserIds" :key="userId">{{ userId }}</li>
+            <li v-for="user in this.connectedUserIds.filter(id => id !== userId)" :key="user"
+                @click="storeSendingToId(user)">{{ user }}</li>
         </ul>
+
+        <div id="chat">
+            <div v-if="privateChats[receiverId]" class="privateChatBox">
+
+
+                <div v-for="(message, index) in privateChats[receiverId]" :key="index">
+                    <div v-if="message.senderId === userId" class="senderMessageBox">
+                        <p> {{ message.text }}</p>
+                    </div>
+                    <div v-else class="recieverMessageBox">
+                        <p><strong>DE {{ message.receiverId }} {{ message.text }}</strong></p>
+                        
+                    </div>
+                </div>
+
+            </div>
+
+            <form @submit.prevent="sendPrivateMessage">
+                <input v-model="newMessage" type="text" placeholder="Write your message here..." />
+                <button type="submit">Send</button>
+            </form>
+        </div>
     </div>
 </template>
 
@@ -13,29 +36,133 @@ export default {
     data() {
         return {
             connectedUserIds: [],
+            newMessage: '',
+            userId: '',
+            privateChats: {},
+            receiverId: '',
         };
     },
     created() {
         if (process.client) {
-            // Generate a unique userId for this tab
-            const userId = Math.random().toString(36).substring(2);
+            this.userId = socket.id;
+            socket.emit('userJoined', socket.id);
 
-            // Emit the 'userJoined' event with the userId
-            socket.emit('userJoined', userId);
-
-            this.updateConnectedUsers();
-        }
-    },
-    methods: {
-        updateConnectedUsers() {
             socket.on('updateConnectedUsers', (userIds) => {
                 this.connectedUserIds = userIds;
             });
+
+            socket.on('newMessage', (message) => {
+                this.messages.push(message);
+            });
+
+            socket.on('privateMessageReceived', (message, senderId) => {
+                if (!this.privateChats[senderId] || !this.privateChats[senderId].some(chat => chat.text === message && chat.senderId === senderId)) {
+                    if (!this.privateChats[senderId]) {
+                        this.privateChats[senderId] = [];
+                    }
+                    this.privateChats[senderId].push({ text: message, senderId: senderId });
+                }
+            });
+
+            console.log('socketid   ' + this.userId);
         }
     },
+    methods: {
+        storeSendingToId(id) {
+            this.receiverId = id;
+        },
+        sendPrivateMessage() {
+            if (!this.receiverId || this.newMessage.trim() === '') {
+                alert('Please select a user and enter a non-empty message.');
+                return;
+            }
+
+            const message = this.newMessage;
+
+            if (!this.privateChats[this.receiverId]) {
+                this.privateChats[this.receiverId] = [];
+            }
+            this.privateChats[this.receiverId].push({ text: message, senderId: socket.id });
+
+            socket.emit('sendPrivateMessage', message, this.receiverId, socket.id);
+
+            this.newMessage = '';
+        }
+    }
 };
 </script>
 
+
+
+
+
 <style scoped>
-/* Add your styles here */
+
+
+#container {
+    display: flex;
+    flex-direction: column;
+    height: 100%;
+}
+
+#chat {
+    flex: 1;
+    padding: 4px;
+}
+
+.privateChatBox {
+    overflow-y: auto;
+    max-height: calc(100vh - 200px); /* Adjust height as needed */
+}
+
+.senderMessageBox,
+.recieverMessageBox {
+    max-width: 100%;
+    padding: 4px;
+    justify-content: center;
+
+    margin: 10px;
+    border-radius: 8px;
+    margin-bottom: 10px;
+}
+
+.senderMessageBox {
+    background-color: #ccccccd7;
+    color: black;
+    
+   
+}
+
+.recieverMessageBox {
+    background-color: #cccccc46;
+    align-self: flex-end;
+}
+
+input[type="text"] {
+    width: 100%;
+    padding: 10px;
+    border: 1px solid #ddd;
+    border-radius: 20px;
+    margin-bottom: 10px;
+}
+
+button[type="submit"] {
+    width:100%;
+    height: 40px;
+    background-color: #25D366;
+    color: #fff;
+    border: none;
+    border-radius: 20px;
+    cursor: pointer;
+}
+
+ul li {
+    padding: 10px;
+    
+    cursor: pointer;
+    list-style: none;
+    text-align: left;
+}
+
+
 </style>
--- frontend/server.js
+++ frontend/server.js
@@ -3,7 +3,6 @@ import { createServer } from "http";
 import { Server } from "socket.io";
 import cors from "cors";
 
-
 const app = express();
 
 const corsOptions = {
@@ -18,21 +17,45 @@ const io = new Server(server, {
   cors: corsOptions,
 });
 
-let users = [];
+let users = {};
+
 io.on('connection', (socket) => {
-  // Ignore connections from Nuxt HMR
   if (socket.request.headers.referer && socket.request.headers.referer.includes('hot-update.json')) {
     return;
   }
 
-  console.log('CONNECTION:', {
-    id: socket.id,
-    connected: socket.connected,
+  socket.on('userJoined', (userId) => {
+    console.log('CONNECTION:', {
+      id: socket.id,
+      connected: socket.connected,
+    });
+
+    users[userId] = socket.id; 
+    io.emit('updateConnectedUsers', Object.keys(users));
   });
 
-  socket.on('userJoined', (userId) => {
-    users[userId] = socket.id;
-    io.emit('updateConnectedUsers', Object.keys(users));
+  socket.on('sendPrivateMessage', (message, receiverId, senderId) => {
+    const receiverSocketId = users[receiverId];
+    const senderSocketId = users[senderId];
+    console.log('sendPrivateMessage:', {
+      message,
+      receiverId,
+      senderId,
+      receiverSocketId,
+      senderSocketId,
+    });
+    if (!receiverSocketId) {
+      console.error('Receiver not found');
+      return;
+    }
+
+    if (!senderSocketId) {
+      console.error('Sender not found');
+      return;
+    }
+
+    io.to(senderSocketId).emit('privateMessageSent', message, receiverId, senderId);
+    io.to(receiverSocketId).emit('privateMessageReceived', message, senderId, receiverId);
   });
 
   socket.on('disconnect', () => {
@@ -42,10 +65,16 @@ io.on('connection', (socket) => {
     });
 
     // Remove the disconnected user
-    users = Object.fromEntries(Object.entries(users).filter(([userId, socketId]) => socketId !== socket.id));
+    for (const [key, value] of Object.entries(users)) {
+      if (value === socket.id) {
+        delete users[key];
+        break;
+      }
+    }
     io.emit('updateConnectedUsers', Object.keys(users));
   });
 });
+
 
 server.listen(5000, () => {
   console.log('server running at http://localhost:5000');
