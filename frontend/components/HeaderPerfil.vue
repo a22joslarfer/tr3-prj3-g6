0da50678f79665a5acc7d5bdad64ef2a3fd63ead
a20rubdelsan <a20rubdelsan@inspedralbes.cat>
2024-04-24 13:15:02 +0200

    perfil

---
+++ frontend/components/HeaderPerfil.vue
@@ -0,0 +1,64 @@
+<template>
+    <header class="profile-header">
+        <button @click="goBack" class="back-button">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor"
+                class="back-icon">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 19l-7-7m0 0l7-7m-7 7h18" />
+            </svg>
+        </button>
+        <h1 class="profile-title">{{ pageTitle }}</h1>
+    </header>
+</template>
+
+<script>
+export default {
+    props: {
+        pageTitle: {
+            type: String,
+            default: 'Ajustes'
+        }
+    },
+    methods: {
+        goBack() {
+            // Aquí puedes agregar la lógica para ir hacia atrás
+            this.$router.go(-1); // Esto es un ejemplo para volver a la página anterior, ajusta según necesites
+        }
+    }
+}
+</script>
+
+<style scoped>
+.profile-header {
+    display: flex;
+    align-items: center;
+    padding: 12px;
+    background-color: #ffffff;
+    border-bottom: 1px solid #eaeaea;
+    margin-bottom: 20px;
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+}
+
+.back-button {
+    background: none;
+    border: none;
+    padding: 0;
+    margin-right: 10px;
+    cursor: pointer;
+    display: grid;
+    justify-content: start;
+}
+
+.back-icon {
+    width: 24px;
+    height: 24px;
+    color: #333333;
+}
+
+.profile-title {
+    font-size: 18px;
+    margin: 0;
+    color: #333333;
+    text-align: center;
+}
+</style>
---
+++ frontend/pages/PERFIL/logout.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    <div class="container">
+      <div class="title">Cerrar Sesión</div>
+      <button @click="logout" class="button">Cerrar Sesión</button>
+    </div>
+  </div>
+</template>
+
+<script>
+import { useStore } from '../stores/index.js';
+
+export default {
+  mounted() {
+    const store = useStore();
+    console.log(store); // Verifica que la tienda se esté utilizando correctamente
+  },
+  methods: {
+    logout() {
+      const store = useStore();
+      store.reset_booking_info();
+      localStorage.removeItem('token');
+      this.$router.push('/login');
+    }
+  }
+};
+</script>
+
+<style scoped>
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh;
+  background-color: #eff4f3;
+}
+
+.title {
+  font-size: 28px;
+  font-weight: bold;
+  color: #333;
+  margin-bottom: 20px;
+  text-align: center;
+}
+
+.button {
+  width: 200px;
+  height: 50px;
+  border: none;
+  border-radius: 25px;
+  background-color: #ff806d;
+  color: white;
+  font-size: 18px;
+  font-weight: bold;
+  cursor: pointer;
+}
+</style>
