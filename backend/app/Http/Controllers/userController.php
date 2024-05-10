4537fbe8e39fdf108f0b06627d28b92d18d85666
a22joslarfer <a22joslarfer@inspedralbes.cat>
2024-05-10 08:33:41 +0200

    arreglar error merge qr controller no actualitzat

--- backend/app/Http/Controllers/userController.php
+++ backend/app/Http/Controllers/userController.php
@@ -21,45 +21,47 @@ class UserController extends Controller
     }
 
     public function store(Request $request)
-{
-    // Validar los datos del formulario
-    $request->validate([
-        'name' => 'required|string',
-        'email' => 'required|email|unique:users,email',
-        'password' => 'required|string|min:8',
-        'phone' => 'nullable|string', // Añadido
-        'birthday' => 'nullable|date', // Añadido
-        'profile_photo' => 'nullable|image|mimes:jpeg,png,jpg,gif|max:2048', // Asegúrate de agregar la validación de la imagen aquí
-    ]);
+    {
+        // Validar los datos del formulario
+        $request->validate([
+            'name' => 'required|string',
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required|string|min:8',
+            'phone' => 'nullable|string', // Añadido
+            'birthday' => 'nullable|date', // Añadido
+            'profile_photo' => 'nullable|image|mimes:jpeg,png,jpg,gif|max:2048', // Asegúrate de agregar la validación de la imagen aquí
+        ]);
 
-    // Procesar y almacenar la imagen de perfil
-    $profilePhotoPath = null;
-    if ($request->hasFile('profile_photo')) {
-        // Obtener el nombre del archivo
-        $fileName = $request->file('profile_photo')->getClientOriginalName();
-        
-        // Mover la imagen a la carpeta profile en el directorio public
-        $request->file('profile_photo')->move(public_path('profile'), $fileName);
-    
-        // Almacenar la ruta relativa de la imagen en la base de datos
-        $profilePhotoPath = 'profile/' . $fileName;
+        // Procesar y almacenar la imagen de perfil
+        $profilePhotoPath = null;
+        if ($request->hasFile('profile_photo')) {
+            // Obtener el nombre del archivo
+            $fileName = $request->file('profile_photo')->getClientOriginalName();
+
+            // Mover la imagen a la carpeta profile en el directorio public
+            $request->file('profile_photo')->move(public_path('profile'), $fileName);
+
+            // Almacenar la ruta relativa de la imagen en la base de datos
+            $profilePhotoPath = 'profile/' . $fileName;
+        }
+
+        // Crear un nuevo usuario
+        $user = new User([
+            'name' => $request->input('name'),
+            'email' => $request->input('email'),
+            'password' => Hash::make($request->input('password')),
+            'phone' => $request->input('phone'),
+            'birthday' => $request->input('birthday'),
+            'api_token' => Str::random(60),
+            'profile_photo' => $profilePhotoPath, // Almacenar la ruta relativa de la imagen en la base de datos
+        ]);
+        $user->save();
+
+        return response()->json($user, 201);
     }
-    
-    // Crear un nuevo usuario
-    $user = new User([
-        'name' => $request->input('name'),
-        'email' => $request->input('email'),
-        'password' => Hash::make($request->input('password')),
-        'phone' => $request->input('phone'),
-        'birthday' => $request->input('birthday'),
-        'api_token' => Str::random(60), 
-        'profile_photo' => $profilePhotoPath, // Almacenar la ruta relativa de la imagen en la base de datos
-    ]);
-    $user->save();
 
-    return response()->json($user, 201);
-}
-public function show($id)
+
+    public function show($id)
     {
         // Mostrar un usuario específico
         $user = User::findOrFail($id);
@@ -79,10 +81,10 @@ public function show($id)
             'birthday' => 'required|string',
             'old_password' => 'required|string', // Nuevo campo para la contraseña anterior
         ]);
-    
+
         // Obtener el usuario existente
         $user = User::findOrFail($id);
-    
+
         // Verificar si la contraseña anterior coincide
         if (Hash::check($request->input('old_password'), $user->password)) {
             // La contraseña anterior coincide, procede a actualizar el usuario
@@ -95,7 +97,7 @@ public function show($id)
             $user->phone = $request->input('phone');
             $user->birthday = $request->input('birthday');
             $user->save();
-    
+
             return response()->json($user);
         } else {
             // La contraseña anterior no coincide, devuelve un error
@@ -213,50 +215,48 @@ public function show($id)
     }
 
 
-
+    public function generateQRCode($userId)
+    {
+        try {
+            // Contenido del código QR con el ID de usuario de A y la URL de redirección
+            $qrContent = 'http://localhost:3000/auth/' . $userId;    
+            // Generar el código QR con Simple-QRCode
+            $qrCode = QrCode::size(200)->generate($qrContent);
     
-  public function generateQRCode(User $user)
-{
-    try {
-      
-        // Generar el contenido para el código QR (puede ser dinámico) llevar a http://localhost:3000/
-        $qrContent = 'http://viaegis.daw.inspedralbes.cat/';
-
-        // Generar el código QR con Simple-QRCode
-        $qrCode = QrCode::size(200)->generate($qrContent);
-
-        // Nombre del archivo
-        $fileName = 'qr_code_' . time() . '.svg';
-
-        // Ruta de destino para guardar el código QR
-        $qrPath = public_path('qr_codes/' . $fileName);
-
-        // Guardar el código QR en la carpeta de destino
-        File::put($qrPath, $qrCode);
-
-        // Devolver la URL del código QR guardado
-        return response()->file($qrPath);
-        
-    } catch (\Exception $e) {
-        // Manejar el error de generación del código QR
-        return response()->json(['error' => $e->getMessage()], 500);
+            // Nombre del archivo
+            $fileName = 'qr_code_' . time() . '.svg';
+    
+            // Ruta de destino para guardar el código QR
+            $qrPath = public_path('qr_codes/' . $fileName);
+    
+            // Guardar el código QR en la carpeta de destino
+            File::put($qrPath, $qrCode);
+    
+            // Devolver la URL del código QR guardado
+            return response()->file($qrPath);
+        } catch (\Exception $e) {
+            // Manejar el error de generación del código QR
+            return response()->json(['error' => $e->getMessage()], 500);
+        }
     }
-}
+    
+
 
     // return profile_photo from user
-    public function getProfilePhoto($id){
-    $user = User::find($id);
-    if ($user) {
-        // Construir la URL completa de la imagen de perfil
-        $profilePhotoUrl = url($user->profile_photo);
-        return response()->json($profilePhotoUrl);
-    }
-    return response()->json(['error' => 'User not found'], 404);
-}
-public function getFollowers($id)
+    public function getProfilePhoto($id)
     {
         $user = User::find($id);
-        
+        if ($user) {
+            // Construir la URL completa de la imagen de perfil
+            $profilePhotoUrl = url($user->profile_photo);
+            return response()->json($profilePhotoUrl);
+        }
+        return response()->json(['error' => 'User not found'], 404);
+    }
+    public function getFollowers($id)
+    {
+        $user = User::find($id);
+
         if ($user) {
             $followersCount = $user->seguidores;
             return response()->json(['seguidores' => $followersCount]);
@@ -264,6 +264,4 @@ public function getFollowers($id)
 
         return response()->json(['error' => 'User not found'], 404);
     }
-
 }
-
