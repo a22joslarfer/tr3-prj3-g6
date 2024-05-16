93b60d7a7e858d21f4733209689a59fd3be6f7cd
a22joslarfer <a22joslarfer@inspedralbes.cat>
2024-05-16 12:13:34 +0200

    create queda

--- frontend/pages/crud/discotecas/create.vue
+++ frontend/pages/crud/discotecas/create.vue
@@ -1,46 +1,13 @@
 <template>
     <div class="container mt-4">
         <form @submit.prevent="crearNuevoItem" class="mb-4">
-            <div class="row">
-                <div class="col-md-6">
 
-                    <div class="form-group">
-                        <label for="lat">Latitude</label>
-                        <input type="number" step="any" class="form-control" id="lat" v-model="nuevoItem.lat" required>
-                    </div>
-                    <div class="form-group">
-                        <label for="minEdad">Minimum Age</label>
-                        <input type="number" class="form-control" id="minEdad" v-model="nuevoItem.minEdad" required>
-                    </div>
-                </div>
-                <div class="col-md-6">
-                    <div class="form-group">
-                        <label for="lng">Longitude</label>
-                        <input type="number" step="any" class="form-control" id="lng" v-model="nuevoItem.lng" required>
-                    </div>
-                    <div class="form-group">
-                        <label for="horario">Schedule</label>
-                        <input type="text" class="form-control" id="horario" v-model="nuevoItem.horario" required>
-                    </div>
-                    <div class="form-group">
-                        <label for="nombre_local">Name</label>
-                        <input type="text" class="form-control" id="nombre_local" v-model="nuevoItem.nombre_local"
-                            required>
-                    </div>
-                </div>
-            </div>
-            <div class="form-group">
-                <label for="telefono">Phone Number</label>
-                <input type="tel" class="form-control" id="telefono" v-model="nuevoItem.telefono" required>
-            </div>
-            <div class="form-group">
-                <label for="descripcion">Description</label>
-                <textarea class="form-control" id="descripcion" rows="3" v-model="nuevoItem.descripcion"
-                    required></textarea>
-            </div>
-            <button type="submit" class="btn btn-primary mt-2">Create</button>
+
         </form>
     </div>
+
+
+
 </template>
 
 
@@ -63,7 +30,8 @@ export default {
                 horario: '',
                 nombre_local: '',
                 telefono: '',
-                descripcion: ''
+                descripcion: '',
+                imgUrl: ''
             }
 
         };
