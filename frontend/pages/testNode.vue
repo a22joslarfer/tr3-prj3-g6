698a2a4a5c71d35af677522fa7c3002fa459ac18
a22joslarfer <a22joslarfer@inspedralbes.cat>
2024-05-24 10:59:24 +0200

    proba node

---
+++ frontend/pages/testNode.vue
@@ -0,0 +1,27 @@
+<template>
+    
+    <div>
+        2
+        {{socket.id}}
+    </div>
+</template>
+
+<script>
+import { socket } from "../../socket.js";
+
+export default {
+    data() {
+        return {
+            socket: socket,
+            id: socket.id
+        };
+    },
+}
+
+console.log('socket' + socket);
+console.log('id' + socket.id);
+
+
+</script>
+
+<style lang="scss" scoped></style>
